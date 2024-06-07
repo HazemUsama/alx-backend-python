6d882ac87d4a7cc3c5cc24fafea3d236d9caf177
HazemUsama <hazemosama681@gmail.com>
2024-06-07 04:57:40 +0300

    mixed list

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Complex types - mixed list"""
+from typing import List
+
+
+def sum_mixed_list(mxd_lst: List[int, float]) -> float:
+    """
+    mxd_lst: list of ints and floats
+
+    return: the sum of the list
+    """
+    return sum(mxd_lst)
+
