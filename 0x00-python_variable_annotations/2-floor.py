2099045f973c802e68fa51c0be5e73600180a15c
HazemUsama <hazemosama681@gmail.com>
2024-06-07 04:45:00 +0300

    floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Basic annotations - floor"""
+
+
+def floor(n: float) -> int:
+    """
+    n: float
+
+    return: n floor
+    """
+    import math
+    return math.floor(n)
