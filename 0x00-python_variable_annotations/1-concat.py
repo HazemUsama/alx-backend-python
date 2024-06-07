4a5d9662040ac54267ec2cd43a57d58522194878
HazemUsama <hazemosama681@gmail.com>
2024-06-07 04:41:58 +0300

    concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Basic annotations - concat"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    str1: first string
+    str2: second string
+
+    return: concatenate str1 + str2
+    """
+    return str1 + str2
