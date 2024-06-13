b450e43ad0aea96a6328b53982ddd7aa33d90016
HazemUsama <hazemosama681@gmail.com>
2024-06-13 23:08:32 +0300

    use random.uniform

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -4,12 +4,12 @@ import asyncio
 import random
 
 
-async def wait_random(max_delay: int = 10):
+async def wait_random(max_delay: int = 10) -> float:
     """
     max_delay: the max delay the function gonna wait
 
     return: the delay time
     """
-    time = max_delay * random.random()
+    time = random.uniform(0, max_delay)
     await asyncio.sleep(max_delay * random.random())
     return time
