312c716a86af8f7a179b7a09f2d4ccf3cf4697ae
HazemUsama <hazemosama681@gmail.com>
2024-06-14 23:45:38 +0300

    add return type

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,9 +2,10 @@
 """Async Generator"""
 import asyncio
 from random import random
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     """wait 1 second, then yield a random number between 0 and 10"""
     for _ in range(10):
         await asyncio.sleep(1)
