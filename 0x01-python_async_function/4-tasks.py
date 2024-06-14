b8ef88d3e8ca31045099748c13ee1ba65042afe5
HazemUsama <hazemosama681@gmail.com>
2024-06-14 23:24:14 +0300

    another easy fix

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -2,7 +2,7 @@
 """Wait N Random Time but with a twist"""
 import asyncio
 from typing import List
-task_wait_random = __import__('3-task').task_wait_random
+task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
