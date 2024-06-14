f7a51a51155c1d0572b717b98081c8ceb2188ba2
HazemUsama <hazemosama681@gmail.com>
2024-06-14 23:22:44 +0300

    all done

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Wait N Random Time but with a twist"""
+import asyncio
+from typing import List
+task_wait_random = __import__('1-concurrent_coroutines').task_wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    n: the number of random times we are gonna generate
+    max_delay: the max delay the function gonna wait
+
+    return: a list of random times we waited
+    """
+    waited = [task_wait_random(max_delay) for _ in range(n)]
+
+    return [await task for task in asyncio.as_completed(waited)]
