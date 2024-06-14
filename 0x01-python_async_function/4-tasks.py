c4ac5813d50b9b1ca203b069b996117ea1f23c09
HazemUsama <hazemosama681@gmail.com>
2024-06-14 23:26:44 +0300

    stupid fix

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -5,7 +5,7 @@ from typing import List
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> List[float]:
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
     n: the number of random times we are gonna generate
     max_delay: the max delay the function gonna wait
