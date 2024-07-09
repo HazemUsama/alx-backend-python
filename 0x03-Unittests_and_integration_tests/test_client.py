804895352de2cf006ef16e90f0d2ff737b48bca4
HazemUsama <hazemosama681@gmail.com>
2024-07-09 07:22:14 +0300

    Last fix

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 """Test the client module"""
 from client import GithubOrgClient
-from utils import get_json
 from parameterized import parameterized
 import unittest
 from unittest.mock import patch, Mock
@@ -19,9 +18,7 @@ class TestGithubOrgClient(unittest.TestCase):
         """Test the org method of GithubOrgClient"""
         test_class = GithubOrgClient(org_name)
         test_class.org()
-        mock_get.assert_called_once_with(
-            test_class.ORG_URL.format(org=org_name)
-        )
+        mock_get.called_once_with(test_class.ORG_URL.format(org=org_name))
 
 
 if __name__ == "__main__":
